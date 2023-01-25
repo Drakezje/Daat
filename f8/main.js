c19031743f6d6741c9bef251124569abc7ebdd0f
Drakezje <shyndeptrai01@gmail.com>
2023-01-26 02:24:47 +0700

    1

--- f8/main.js
+++ f8/main.js
@@ -32,5 +32,59 @@
  === quan tâm  value và kiểu dữ liệu (nếu mà kết quả phức tạp sẽ kt giá trị tham chiếu vùng nhớ)
  !== ko bằng
 8. truthy bất cứ giá trị chuyển sang boolean là true (một mảng trống, array trống, hàm trống là true)
-9.falsy 
+9.falsy bất cứ giá trị chuyển sang kiểu boolean là false
+    các giá trị được xem là falsy
+    false
+    0
+    "" or ''
+    null
+    undefined
+    Nan
+10.tips convert sang kiểu boolean là !!
+11.ngoại lệ document.all là một đối tượng nhưng khi convert sang boolean là false
+document.all khi là toán hạng của toán tử là so sánh == hoặc != sẽ là undefined
+typeof document.all sẽ trả về "undefined"
+12.toán tử logic
+
+&& tìm phần tử false
+nếu 1 dãy true hết thì trả về phần từ cuối cùng
+nễu 1 dãy có nhiều false thì trả phần tử false đầu tiên rồi dừng
+|| tìm phần tử true
+nếu 1 dãy true hết thì phần tử trả true đầu tiền
+nễu 1 dãy false hết thì phần tử cuối cùng
+các phần từ được xem là false
+0
+false
+'' or ""
+null
+undefined
+Nan
+tóm tắt: tìm từ trái qua phải
+&& tìm false, || tìm true
+
+13. hàm ko trả về mặc địch là undefined
+14. khi function trùng tên, function mới nhất được gọi
+15. các dạng function 
+declaration function 
+function showMessage{}
+expression function dạng gắn function cho 1 biến, có thể có hoặc ko tên function
+var showMessage = function autoShow{}
+setTimeout(function(){})
+var myobject = {
+    myfunction:function(){
+
+    }
+}
+16. có 2 cách tạo chuỗi
+1. var fullname = "ThanhDat"
+var fullname = new string("ThanhDat")
+17. một số case dùng backslash(\)
+gõ dấu văn bản chứa '' or ""
+xuống dòng \n
+gõ dấu \ trong văn bản
+cách 1 khoảng tab \t
+
+18. template es6 cộng chuỗi
+console.log(`tổi là : ${frist name} + ${last name}`)
+
 */
